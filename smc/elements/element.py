f33350c76640a7e86ce80523fb41b012de470776
David LePage <dwlepage70@gmail.com>
2016-05-16 22:07:32 -0500

    split node and interface creation

--- smc/elements/element.py
+++ smc/elements/element.py
@@ -1,13 +1,12 @@
 import smc.helpers
-from pprint import pprint
 
 class SMCElement(object):
     def __init__(self):
-        self.json = None
-        self.etag = None
-        self.type = None
-        self.name = None
-        self.href = None
+        self.json = None    #required for post/put
+        self.etag = None    #required for put
+        self.type = None    #required
+        self.name = None    #required
+        self.href = None    #required
         self.comment = None
 
     def create(self):
@@ -21,6 +20,7 @@ class SMCElement(object):
     
     def __str__(self):
         return "name: %s, type: %s" % (self.name, self.type)
+    #    return "name: %s" % self.name
  
         
 class Host(SMCElement):
@@ -137,6 +137,7 @@ class Route(SMCElement):
     def create(self):
         routing = smc.helpers.get_json_template('routing.json')
         #Next Hop Gateway
+        #self.json holds original routing table
         routing['gateway']['href'] = self.gw_href
         routing['gateway']['ip'] = self.gw_ip
         routing['gateway']['name'] = self.gw_name
@@ -162,123 +163,153 @@ class Route(SMCElement):
     def __str__(self):
         return "name: %s, type: %s, gw: %s, net: %s, int_id: %s" % (self.name, self.type, self.gw_name, self.network_ip, self.interface_id)
     
-    
-class SingleFW(SMCElement):
-    """ Create initial single l3 fw 
-    By default currently set interface 0 to be the default management port. 
-    """
-    def __init__(self):
-        SMCElement.__init__(self)
-        self.mgmt_ip = None
-        self.mgmt_network = None
-        self.log_server = None
-        self.dns = None
-        self.fw_license = None
-        self.interface_id = None
-        
-    def create(self):      
-        single_fw = smc.helpers.get_json_template('single_fw.json')
-        
-        for k in single_fw:
-            if k == 'name':
-                single_fw[k] = self.name
-            elif k == 'nodes':
-                single_fw[k][0]['firewall_node']['name'] = self.name + ' node 1'
-            elif k == 'physicalInterfaces':               
-                single_fw[k][0]['physical_interface']['interfaces'][0]['single_node_interface']['address'] = self.mgmt_ip
-                single_fw[k][0]['physical_interface']['interfaces'][0]['single_node_interface']['network_value'] = self.mgmt_network
-            elif k == 'log_server_ref':
-                single_fw[k] = self.log_server
-            elif k == 'domain_server_address':
-                if self.dns:
-                    single_fw[k].append({"rank": 0, "value": self.dns})
-        
-        self.json = single_fw
-        return self  
-                
 
-class L3interface(SMCElement):
-    """ Add physical interface to firewall
-    
-    If interface_id is not set, the next sequential interface id will be used
-    
-    Implementation detail: mgmt is always installed to interface 0. If the
-    operator specifies to add an interface at pos 5 (for example) and adds 
-    additional interfaces without specifying position, it will always be +1 of
-    the highest interface defined.   
-    """
+
+class EngineNode(SMCElement):
     def __init__(self):
         SMCElement.__init__(self)
-        self.ip = None
-        self.mask = None
-        self.interface_id = None
+        self.dns = []
+        self.log_server = None
+        self.interfaces = []
         
-    def create(self):       
-        interface = smc.helpers.get_json_template('routed_interface.json')
-        interface_ids = [] #store existing node_id to find next open
+    def create(self):
+        self.json = {
+            "name": self.name,
+            "nodes": [],
+            "domain_server_address": [],
+            "log_server_ref": self.log_server,
+            "physicalInterfaces": self.interfaces
+        }
+        self.engine = {
+            self.type: {
+                "activate_test": True,
+                "disabled": False,
+                "loopback_node_dedicated_interface": [],
+                "name": self.name + " node 1 ",
+                "nodeid": 1
+                }
+        }
+        if len(self.dns) > 0:
+            rank_i = 0
+            for entry in self.dns:
+                self.json['domain_server_address'].append({"rank": rank_i, "value": entry})
+        self.json['nodes'].append(self.engine)
+        return self
+
+    def update(self, original_cfg):
+        self.json = original_cfg
+        for interface in self.interfaces:
+            self.json['physicalInterfaces'].append(interface)
+        return self
+ 
+                   
+class IPS(EngineNode):
+    def __init__(self):
+        EngineNode.__init__(self)
+        self.type = "ips_node"
+ 
         
-        if self.interface_id is None:
-            for node_interface in self.json['physicalInterfaces']:
-                interface_ids.append(node_interface['physical_interface']['interface_id'])
+class FWLayer2(EngineNode):
+    def __init__(self):
+        EngineNode.__init__(self)
+        self.type = "fwlayer2_node"
+ 
+                           
+class L3FW(EngineNode):
+    def __init__(self):
+        EngineNode.__init__(self)
+        self.type = "firewall_node"
+
         
-                interface_id = [int(i) for i in interface_ids]  #needed to find max
-                self.interface_id = max(interface_id)+1
+class Interface(object):
+    def __init__(self):
+        self.interface_id = '0'
+        self.nicid = '0' #should match interface_id
+        self.nodeid = '1'
+        self.json = None
+        self.config = smc.helpers.get_json_template('interface.json')
         
-        phys_iface = interface['physical_interface']
-        phys_iface['interface_id'] = str(self.interface_id)
-        iface = interface['physical_interface']['interfaces'][0]
-        iface = iface['single_node_interface']
-        iface['address'] = self.ip
-        iface['nicid'] = str(self.interface_id)
-        iface['network_value'] = self.mask
+    def create(self):
+        #assemble full interface
+        self.json = self.config['physical_interface']
+        self.json['physical_interface']['interface_id'] = self.interface_id
+        self.json['physical_interface']['interfaces'].append(self.int_json)
+       
         
-        self.json['physicalInterfaces'].append(interface)
+class SingleNodeInterface(Interface):
+    def __init__(self):
+        Interface.__init__(self)
+        self.type = "single node interface"
+        self.address = None
+        self.network_value = None
+        self.auth_request = False
+        self.primary_mgt = False
+        self.outgoing = False
         
-        return self        
-    
-    def __str__(self):
-        return "name: %s, type: %s, ip: %s, mask: %s, int_id: %s" % \
-            (self.name, self.type, self.ip, self.mask, self.interface_id)   
+    def create(self):
+        self.int_json = self.config['single_node_interface']
+        self.int_json['single_node_interface']['address'] = self.address
+        self.int_json['single_node_interface']['network_value'] = self.network_value
+        self.int_json['single_node_interface']['nodeid'] = self.nodeid
+        self.int_json['single_node_interface']['nicid'] = self.nicid
+        self.int_json['single_node_interface']['auth_request'] = self.auth_request
+        self.int_json['single_node_interface']['primary_mgt'] = self.primary_mgt
+        self.int_json['single_node_interface']['outgoing'] = self.outgoing
+        super(SingleNodeInterface, self).create()
+
+
+class NodeInterface(Interface):
+    def __init__(self):
+        Interface.__init__(self)
+        self.type = "node interface"
+        self.address = None
+        self.network_value = None
+        self.auth_request = False
+        self.primary_mgt = False
+        self.outgoing = False
+        
+    def create(self):
+        self.int_json = self.config['node_interface']
+        self.int_json['node_interface']['address'] = self.address
+        self.int_json['node_interface']['network_value'] = self.network_value
+        self.int_json['node_interface']['nodeid'] = self.nodeid
+        self.int_json['node_interface']['nicid'] = self.nicid
+        self.int_json['node_interface']['auth_request'] = self.auth_request
+        self.int_json['node_interface']['primary_mgt'] = self.primary_mgt
+        self.int_json['node_interface']['outgoing'] = self.outgoing
+        super(NodeInterface, self).create()
+        
+        
+class InlineInterface(Interface):
+    def __init__(self):
+        Interface.__init__(self)
+        self.type = "inline interface"
+        self.logical_interface_ref = None
+        
+    def create(self):
+        self.int_json = self.config['inline_interface']
+        self.int_json['inline_interface']['logical_interface_ref'] = self.logical_interface_ref
+        self.int_json['inline_interface']['nicid'] = self.nicid
+        super(InlineInterface, self).create()
 
                
-class L2FW(SMCElement):
-    def __init__(self, SMCElement=None):
-        SMCElement.__init__(self)
-        self.mgmt_ip = None
-        self.mgmt_network = None
-        self.log_server = None
-        self.dns = None
-        self.fw_license = None
-        self.log_server = None
-        self.inline_pair = None
-        self.logical_interface = None
+class CaptureInterface(Interface):
+    def __init__(self):
+        Interface.__init__(self)
+        self.type = "capture interface"
+        self.logical_interface_ref = None
         
-    def create(self):        
-        self.json = smc.helpers.get_json_template('layer2_fw.json')
-        
-        for k in self.json:
-            if k == 'name':
-                self.json[k] = self.name
-            elif k == 'nodes':
-                self.json[k][0]['fwlayer2_node']['name'] = self.name + ' node 1'
-            elif k == 'physicalInterfaces':               
-                mgmt_int = self.json[k][0]
-                mgmt_int['physical_interface']['interfaces'][0]['node_interface']['address'] = self.mgmt_ip
-                mgmt_int['physical_interface']['interfaces'][0]['node_interface']['network_value'] = self.mgmt_network
-            elif k == 'log_server_ref':
-                self.json[k] = self.log_server
-            elif k == 'domain_server_address':
-                if self.dns:
-                    self.json[k].append({"rank": 0, "value": self.dns})
-                           
-        self.json['physicalInterfaces'].append(self.inline_pair)
-                
-        return self
+    def create(self):
+        self.int_json = self.config['capture_interface']
+        self.int_json['capture_interface']['logical_interface_ref'] = self.logical_interface_ref
+        self.int_json['capture_interface']['nicid'] = self.nicid
+        super(CaptureInterface, self).create()
 
 
 class Logicalinterface(SMCElement):
     def __init__(self, SMCElement=None):
         SMCElement.__init__(self)
+        self.type = "logical interface"
     
     def create(self):
         self.json = {
@@ -287,33 +318,7 @@ class Logicalinterface(SMCElement):
         }
     
         return self
-    
-class L2interface(SMCElement):
-    def __init__(self, SMCElement=None):
-        SMCElement.__init__(self)
-        self.interface_id = []
-        self.logical_int_href = None
-        
-    def create(self):
-        interface = smc.helpers.get_json_template('inline_interface.json')
-        
-        int_str = [str(i) for i in self.interface_id]
-        
-        phys_int = interface['physical_interface']
-        phys_int['interface_id'] = self.interface_id[0]
-        inline_int = interface['physical_interface']['interfaces'][0]['inline_interface']
-        inline_int['logical_interface_ref'] = self.logical_int_href
-        inline_int['nicid'] = ('-').join(int_str)
-        
-        if self.json is not None: #modify existing l2fw
-            self.json['physicalInterfaces'].append(interface)
-        else:   #add to new l2fw (required by l2fw to have at least one inline pair)
-            self.json = interface
-        
-        return self
-        
-        
-        
+
         
         
         
