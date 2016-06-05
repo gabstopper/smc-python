5859e197cd4ef53d0f2590955b801c554044b7cb
David LePage <dwlepage70@gmail.com>
2016-06-05 18:46:42 -0500

    need to find a better way to return text to smc_cli

--- smc/actions/show.py
+++ smc/actions/show.py
@@ -3,61 +3,72 @@ from smc.actions import search
 from smc.api import web as web_api
 from pprint import pprint
 
+logger = logging.getLogger(__name__)
 
 class ElementContainer(object):
     def __init__(self, name=None):
         self.name = name
         self.type = None #list of available types?
-        self.json = None
+        self.json = []
+        self.headers = []
+        self.elements = []
         
-    def showKeys(self):
-        #return all_option_names()
-        pass      
+    def showKeys(self): 
+        pass
 
-    def showByName(self, name):
-        t = search.element_info_as_json(name)
-        self.name = name
-        if t:
-            if t.get('type'): self.type = t.get('type', None)
-            print "Name: %s, Type: %s" % (self.name, self.type)
-            self.json = search.element_as_json(self.name)
-            print self.json
-                                                    
-    def showByType(self, element):
-        self.type = element
-        self.json = search.all_elements_by_type(element)
-        if self.json:
-            print "{:<45} {:<15}".format('Name', 'Type')
-            for result in self.json:
-                print "{:<45} {:<15}".format(result.get('name', None), result.get('type', None))
+    def single_fw(self, name=None, all=False):
+        self._engine('single_fw', name, all)
+        
+    def single_ips(self, name=None, all=False):
+        self._engine('single_ips', name, all)
+    
+    def single_layer2(self, name=None, all=False):
+        self._engine('single_layer2', name, all)
                 
-                    
+    def _engine(self, engine_type, name=None, all=False):
+        if all:
+            self.showByType(engine_type)
+        elif name:
+            self.showByName(name)
+            t = search.element_as_json(name)
+            if t: pprint(t)
+                                                
+    def showByName(self, name):
+        sb = []
+        ignore = ['key','link']
+        t = search.element_as_json(name)
+        if t:
+            for entry, value in t.iteritems():
+                if entry not in ignore:
+                    sb.append(entry + ": " + str(value))
+            print '\n'.join(sb)
+                                                               
+    def showByType(self, element):
+        self.headers.extend(['Name', 'Type'])
+        t = search.all_elements_by_type(element)
+        if t:
+            for result in t:
+                self.elements.append([result.get('name', None), result.get('type', None)])
+            print self.__str__()
+    
+    def showAsJson(self, j):
+        pass
+                
     def __str__(self):
         sb = []
-        for key in self.__dict__:
-            sb.append("{key}='{value}'".format(key=key, value=self.__dict__[key]))
- 
-        return ', '.join(sb)
- 
+        col_width = [max(len(x) for x in col) for col in zip(*self.elements)]
+        #print "column max width: %s" % col_width
+        sb.append("".join("{:{}}".format(x, col_width[i] + 5)
+                                    for i, x in enumerate(self.headers)))
+        for entry in self.elements:
+            sb.append("".join("{:{}}".format(x, col_width[i] + 5)
+                                    for i, x in enumerate(entry)))
+                
+        return '\n'.join(sb)
+       
     def __repr__(self):
         return self.__str__() 
     
-    def test(self):
-        d = {1: ["blah", 5]}
-        print "{:<8} {:<15} {:<10}".format('Key','Label','Number')
-        for k, v in d.iteritems():
-            label, num = v
-            print "{:<8} {:<15} {:<10}".format(k, label, num)        
-
-    def test2(self):
-        teams_list = ["Man Utd", "Man City", "T Hotspur"]
-        data = ([[1, 2, 1],
-                 [0, 1, 0],
-                 [2, 4, 2]])
-        row_format ="{:>15}" * (len(teams_list) + 1)
-        print row_format.format("", *teams_list)
-        for team, row in zip(teams_list, data):
-            print row_format.format(team, *row)
     
 if __name__ == "__main__":
     import time
@@ -68,12 +79,17 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s: %(message)s')
     
     element = ElementContainer()
-    #for option in all_option_names():
-    #    print "Looking for host objects: %s" % option
-    #    time.sleep(3)
-    #    element.showByType(option)
     
-    element.showByName('binh')
-    
+    #element.single_fw(name='ewf')
+    element.showByType('single_fw')
+    #element.showByType('router')
+    #element.showByType('network')
+    #element.showByName('sg_vm')
+    #element.single_fw('all')
+    #element.single_ips('all')
+    #element.single_layer2('all')
+    #element.display()
+    #print element
+    #print element.table()
     
     web_api.session.logout()
