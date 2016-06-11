f02d7d5a0956e88c081db165dc66132f5071d64d
David LePage <dwlepage70@gmail.com>
2016-06-11 13:30:56 -0500

    documentation

--- smc/helpers.py
+++ smc/helpers.py
@@ -6,6 +6,11 @@ logger = logging.getLogger(__name__)
 
 
 def get_json_template(template_name):
+    """ Get the json template file specified as template name
+    Used for element templates
+    :param template_name: name of template
+    :return json of template, None if error
+    """
     resource_package = __name__  ## this is so files can be retrieved when in egg
     resource_path = os.path.join('/elements/templates', template_name)
     template = pkg_resources.resource_string(resource_package, resource_path)
@@ -18,6 +23,10 @@ def get_json_template(template_name):
     return v
  
 def is_valid_ipv4(ipaddr):
+    """ Is the ipv4 provided valid
+    :param ipaddr: ip address provided
+    :return True if valid, None otherwise
+    """
     #TODO: python 2.x ipaddress module needs unicode string, python 2 convert from binary 
     try:
         if ipaddress.IPv4Address(ipaddr.decode('utf-8')): #python 2.x
@@ -27,6 +36,11 @@ def is_valid_ipv4(ipaddr):
     return None
     
 def is_ipaddr_in_network(host_ip, network):
+    """ Check if host ip is in the network specified
+    :param host_ip: host ip address provided
+    :param network: network address provided
+    :return True if valid, None if error
+    """
     try:
         if ipaddress.ip_address(host_ip.decode('utf-8')) in ipaddress.ip_network(network.decode('utf-8')):
             return True
@@ -35,14 +49,10 @@ def is_ipaddr_in_network(host_ip, network):
     return None
            
 def ipaddr_as_network(net_addr):
-    """ network can be 255.255.255.0 or /24 formats 
-        Args:
-            * net_addr: network address, either 1.1.1.1/255.255.255.0, or 1.1.1.1./24
-        Returns:
-            Validated address in CIDR format
-            None if invalid
-        Raises: 
-            ValueError if host bits set do not fall into cidr
+    """ Get ipaddr as network cide
+    Network can be 255.255.255.0 or /24 formats 
+    :params net_addr: network address, either 1.1.1.1/255.255.255.0, or 1.1.1.1./24
+    :return Validated address in CIDR format or None if invalid
     """
     try :
         return ipaddress.ip_network(net_addr.decode('utf-8')).exploded
