a6242bca486e5df50e335ee5bafcaa95435019c1
David LePage <dwlepage70@gmail.com>
2016-08-30 10:29:46 -0500

    update setup.py, remove cli dependencies

--- setup.py
+++ setup.py
@@ -5,18 +5,15 @@ def readme():
         return f.read()
     
 setup(name='smc-python',
-      version='0.1',
+      version='0.2',
       description='Python based API to Stonesoft Security Management Center',
       url='http://github.com/gabstopper/smc-python',
       author='David LePage',
       author_email='dwlepage70@gmail.com',
       license='None',
-      packages=['smc', 'smc.actions', 'smc.api', 'smc.elements', 'smc.cli'],
+      packages=['smc', 'smc.actions', 'smc.api', 'smc.elements'],
       install_requires=[
-          'requests',
-          'ipaddress',
-          'prompt-toolkit',
-          'pygments'
+          'requests'
       ],
       include_package_data=True,
       zip_safe=False)
