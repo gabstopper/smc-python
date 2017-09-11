93e44d416297715841b9c589e1b232bd75f07ab2
David LePage <dwlepage70@gmail.com>
2017-09-11 12:33:01 -0500

    mock websocket

--- smc/docs/conf.py
+++ smc/docs/conf.py
@@ -18,6 +18,16 @@
 #
 import os
 import sys
+from mock import Mock as MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+MOCK_MODULES = ['websocket']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 
 #sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../../'))
